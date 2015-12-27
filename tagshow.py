6346b9a026457b43727a923563a1221381a74924
Josh Tanner <mindful.jt@gmail.com>
2015-12-26 19:19:48 -0800

    usability improvements, slight refactoring

--- tagshow.py
+++ tagshow.py
@@ -22,14 +22,14 @@ IMG_DIR = os.getcwd()
 
 def __get_image(name):
     try:
-        dup = (len([img for img in IMAGES if img.path == name]) > 0)
-        #"in" operator wasn't doing the job, the above is hacky (should redefine an operator instead) but quick
-        if not dup:
-            IMAGES.append(Image(name))
-        return True
+        IMAGES.append(Image(name))
     except Exception as e:
         print("Could not get image with name", name)
         print(e)
+
+def __image_is_dup(name):
+    #"in" operator wasn't doing the job, the above is hacky (should redefine an operator instead) but quick
+    return (len([img for img in IMAGES if img.path == name]) > 0)
 
 
 def __get_dir():
@@ -37,12 +37,28 @@ def __get_dir():
                 "."+name.split(".")[-1] in EXTENSIONS]
     for file in files:
         __get_image(file)
+    IMAGES.sort(key=operator.attrgetter('path'))
+
+
+
+def __render(show_name):
+    for img in IMAGES:
+        img.render()
+    with open(show_name+'.html', 'w') as f:
+        f.write(Canvas.render())
+    print("Slideshow named", '"'+show_name+'"', "successfully compiled from ", IMAGES)
+
+def __auto_get_dir_if_fresh():
+    global IMAGES
+    if len(IMAGES) == 0:
+        print("No images selected, defaulting to selecting from directory")
+        __get_dir()
 
 #TODO: we could save the last version and add an undo(), though we'd also have to save the last command so we could undo
 #saves by saving the last version
 #also we could better display changes like [x] -> [y]
 
-def transcribe_function():
+def __transcribe_function():
     frame = inspect.currentframe().f_back
     name = inspect.getframeinfo(frame)[2]
     args, _, _, values = inspect.getargvalues(frame)
@@ -54,78 +70,128 @@ def transcribe_function():
 
 
 
-#TODO: both get and unget could/should take a variable number of args
-def get(name):
-    failures = 0
-    for extension in EXTENSIONS:
-        if __get_image(name+extension):
-            __get_image(name+extension)
-            success = name+extension
-            break
-        else:
-            failures += 1
-            print("Could not find image as "+name+extension)
+def get(*files):
+    """Takes a variable number of files and searches for them in the current directory with all extensions supported by
+    the program. If all files are found, they will be added to the current list of images. Otherwise, an exception is
+    thrown."""
+    file_additions = []
+    for filename in files:
+        failures = 0
+        for extension in EXTENSIONS:
+            name_with_extension = filename+extension
+            if not __image_is_dup(name_with_extension):
+                success = name_with_extension
+                file_additions.append(success)
+                break
+            else:
+                failures += 1
+                print("Could not find image in directory as "+filename+extension)
 
-    if failures == len(EXTENSIONS):
-        raise "Could not find image "+name
-    else:
-        print("Found image as "+success)
+        if failures == len(EXTENSIONS):
+            raise "Could not find image "+filename
+        else:
+            print("Found image as "+success)
+
+    for filename in file_additions:
+        __get_image(filename)
+        print("Getting "+filename)
 
     display()
 
 
-def unget(name):
-    for ext in EXTENSIONS:
-        exname = name+ext
-        for img in IMAGES:
-            if img.path == exname:
-                del IMAGES[IMAGES.index(img)]
+def unget(*files):
+    """Takes a variable number of files and searches for them in the current list of images with all the extensions
+    supported by the program. If all the files are found, they will be removed from the current list of images.
+    Otherwise, an exception is thrown"""
+    file_removals = []
+    for filename in files:
+        failures = 0
+        for ext in EXTENSIONS:
+            name_with_extension = filename+ext
+            for img in IMAGES:
+                if img.path == name_with_extension:
+                    success = name_with_extension
+                    file_removals.append(success)
+                else:
+                    failures += 1
+                    print("Could not find image in image list as "+name_with_extension)
+
+        if failures == len(EXTENSIONS):
+            raise "Could not find image "+filename
+        else:
+            print("Found image as "+success)
+
+    for img_file in file_removals:
+        del IMAGES[IMAGES.index(img_file)]
+        print("Removing "+img_file+" from image list")
 
     display()
 
 #Start transcribed functions -------
 
-def where_has_prop(property_name):
-    transcribe_function()
-    __get_dir()
+def where_has_tag(tag_name):
+    """Selects all images with the given tag from the current list of images, removing images without this tag from
+    the list. If the current list of images is empty, instead selects from the current directory."""
+    __transcribe_function()
+    __auto_get_dir_if_fresh()
     global IMAGES
-    IMAGES = IMAGES.where_has_prop(property_name)
+    IMAGES = IMAGES.where_has_prop(tag_name)
     display()
 
 
-def where_prop_equals(property_name, value):
-    transcribe_function()
-    __get_dir()
+def where_tag_equals(tag_name, value):
+    """Selects all images where the given tag's value is equal to the given input value. from the current list of images,
+    removing any images where this is not true (whether because the tag's value is different, or because they do not have the
+    tag) from the list. If the current list of images is empty, instead selects from the current directory."""
+    __transcribe_function()
+    __auto_get_dir_if_fresh()
     global IMAGES
-    IMAGES = IMAGES.where_prop_equals(property_name, value)
+    IMAGES = IMAGES.where_prop_equals(tag_name, value)
     display()
 
 
-def where_prop_contains(property_name, value):
-    transcribe_function()
-    __get_dir()
+def where_tag_contains(tag_name, value):
+    """Selects all images where the given tag's value contains the given input value (according to python's "in"
+    operator"), from the current list of images, removing any images where this is not true (whether because the tag's
+    value does not contain the given value, or because they do not have the tag) from the list. If the current list
+    of images is empty, instead selects from the current directory."""
+    __transcribe_function()
+    __auto_get_dir_if_fresh()
     global IMAGES
-    IMAGES = IMAGES.where_prop_contains(property_name, value)
+    IMAGES = IMAGES.where_prop_contains(tag_name, value)
     display()
 
 
-def where_prop_greater(property_name, value):
-    transcribe_function()
-    __get_dir()
+def where_tag_greater(tag_name, value):
+    """Selects all images where the given tag's value is greater than the given input value, from the current list of
+    images, removing any images where this is not true (whether because the tag's value is not greater, or because they
+    do not have the tag) from the list. If the current list of images is empty, instead selects from the current
+    directory."""
+    __transcribe_function()
+    __auto_get_dir_if_fresh()
     global IMAGES
-    IMAGES = IMAGES.where_prop_greater(property_name, value)
+    IMAGES = IMAGES.where_prop_greater(tag_name, value)
     display()
 
-def where_prop_lesser(property_name, value):
-    transcribe_function()
-    __get_dir()
+def where_tag_lesser(tag_name, value):
+    """Selects all images where the given tag's value is lesser than the given input value, from the current list of
+    images, removing any images where this is not true (whether because the tag's value is not lesser, or because they
+    do not have the tag) from the list. If the current list of images is empty, instead selects from the current
+    directory."""
+    __transcribe_function()
+    __auto_get_dir_if_fresh()
     global IMAGES
-    IMAGES = IMAGES.where_prop_lesser(property_name, value)
+    IMAGES = IMAGES.where_prop_lesser(tag_name, value)
     display()
 
 
 def slideshow(show_name, include_scripts = True, include_images = True):
-    transcribe_function()
+    """Compiles a slideshow from all the images in the current list of images using the show_name input value. The
+    include_scripts and include_images arguments determine whether or not images and javascript files should be
+    included in the slideshow directory, respectively. This is recommended, though not necessary as the HTML files
+    just need the files to be accessible somehow. The actual slideshow is an HTML file, and the program will also
+    attempt to compile a script that can be used to regenerate a slideshow with the same tags."""
+    __transcribe_function()
     if not os.path.exists(show_name):
         os.mkdir(show_name)
     else:
@@ -139,7 +205,7 @@ def slideshow(show_name, include_scripts = True, include_images = True):
 
     prev_directory = os.getcwd()
     os.chdir(show_name)
-    render(show_name)
+    __render(show_name)
     os.chdir(prev_directory)
     global BUILDER
     BUILDER.output(show_name, os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
@@ -148,14 +214,20 @@ def slideshow(show_name, include_scripts = True, include_images = True):
 #End transcribed functions -------
 
 def get_dir():
+    """Loads every eligible image (as determined by file extensions) in the current directory into the current list
+    of images. Note that in some instances this method can take a little while to return."""
     print("Getting dir...")
     __get_dir()
-    IMAGES.sort(key=operator.attrgetter('path'))
     display()
 
 def image_by_image():
+    """Iterates over ever every image in the current list of images, allowing them to be tagged individually. Tags can be
+    assigned as CSVs with the notation <tag>:<value>, or tags by themself which will default to a value of "True". Note
+    that the <tag> is assumed to be a string and can be unquoted, but <value> must be a valid python value. Raw numbers
+    are fine, but strings must be in quotes. Additionally, note that images processed by this method are saved one by
+    one as they are processed."""
     print("For tags with values, please use the notation <tag>:<value>. Otherwise, a tag by itself will default to"
-          " a value of true. Note that the <tag> is assumed to be a string and can be unquoted, but <value> must be a valid"
+          " a value of True. Note that the <tag> is assumed to be a string and can be unquoted, but <value> must be a valid"
           " python value. Raw numbers are fine, but strings must be in quotes.")
     for img in IMAGES:
         a = input("Please enter tags for image \""+img.path+"\" separted by spaces: ")
@@ -172,6 +244,9 @@ def image_by_image():
 
 
 def get_numbered_prefix(prefix):
+    """Loads every eligible image (as determined by file extensions) with names constituted by the input value
+    immediately followed by a digit into the current list of images. I.E., for an input of "test", this function
+    would load files with names "test1", "test2", etc. through "testN"."""
     files = [name for name in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), name)) and
                 "."+name.split(".")[-1] in EXTENSIONS and re.fullmatch(prefix+'\d+', name.split(".")[0])]
     for file in files:
@@ -180,51 +255,52 @@ def get_numbered_prefix(prefix):
 
 
 def display():
+    """Prints the current list of images."""
     print(IMAGES)
 
 
-def tag(tagname, value=True):
+def tag(tag_name, value=True):
+    """Adds the given tag name as a tag with the given value. If no value for the tag is passed in, it will default to
+     "True" to simply indicate the presence of the tag. Changes not final until saved."""
     for img in IMAGES:
-        img[tagname] = value
+        img[tag_name] = value
     display()
 
 
 def clear_tags():
+    """Clears all tags from all images in the current list of images. Changes not final until saved."""
     for img in IMAGES:
         img.clear()
     display()
 
 
 def clear():
+    """Empties the list of current images. Any unsaved changes to tags will be lost."""
     global IMAGES
     del IMAGES[:]
     display()
 
 def save():
+    """Saves the list of current images to the disk, writing any changes to the images' tags and making them permanent.
+    Note that in some instances this method can take a little while to return."""
     for img in IMAGES:
         img.save_properties()
     print(IMAGES, " successfully saved")
 
+#I don't think there's a viable usecase for the two below methods
 
-def render(show_name):
-    for img in IMAGES:
-        img.render()
-    with open(show_name+'.html', 'w') as f:
-        f.write(Canvas.render())
-    print("Slideshow named", '"'+show_name+'"', "successfully compiled from ", IMAGES)
+# def set_script_dir(name):
+#     """Sets the directory from which .js scripts are pulled when compiling slideshows to the input value."""
+#     if os.path.exists(name):
+#         global SCRIPT_DIR
+#         SCRIPT_DIR = name
+#     else:
+#         raise "Nonexistant directory"
 
-
-
-def set_script_dir(name):
-    if os.path.exists(name):
-        global SCRIPT_DIR
-        SCRIPT_DIR = name
-    else:
-        raise "Nonexistant directory"
-
-def set_img_dir(name):
-    if os.path.exists(name):
-        global IMG_DIR
-        IMG_DIR = name
-    else:
-        raise "Nonexistant directory"
+# def set_img_dir(name):
+#     """Sets the directory from which image files are pulled when compiling slideshows to the input value."""
+#     if os.path.exists(name):
+#         global IMG_DIR
+#         IMG_DIR = name
+#     else:
+#         raise "Nonexistant directory"
