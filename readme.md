faafd8b4fcc53b70bc0a94f6046554f64bc935c7
Joshua Tanner <jotanner@ebay.com>
2019-09-14 19:58:14 -0700

    minor updates, remove javascript

---
+++ readme.md
@@ -0,0 +1,15 @@
+TagShow
+--
+
+TagShow is a simple set of Python scripts intended primarily to help manage
+downloading illustrations from various places (currentl Danbooru and Pixiv)
+and compile them into slideshows. 
+
+
+Dependencies
+-- 
+
+The requirements.txt file includes all of the Python dependencies, but
+TagShow also requires [jQuery](https://jquery.com/) and
+[Galleria](https://galleria.io/) to be present in the `rendering_resources`
+directory. 
--- rendering/display_writer.py
+++ rendering/display_writer.py
@@ -1,35 +1,40 @@
 import os
 import shutil
 
+
 class DisplayWriter:
 
     rendering_resources_foler = 'rendering_resources'
-
-    jquery_file = 'jquery-3.4.1.min.js'
-    galleria_folder = 'galleria-1.5.7'
-
-    jquery_loc = os.path.join(rendering_resources_foler, jquery_file)
-    galleria_loc = os.path.join(rendering_resources_foler, galleria_folder, 'src', 'galleria.js')
-    galleria_theme_loc = os.path.join(rendering_resources_foler, galleria_folder, 'src', 'themes', 'classic')
-    html_template = os.path.join(rendering_resources_foler, 'slideshow_template.html')
-
     image_locator_string = 'images go here'
 
-
+    @staticmethod
+    def find_resource(resource_name):
+        try:
+            return next((x for x in os.listdir("rendering_resources") if resource_name in x))
+        except StopIteration:
+            raise Exception("Unable to find "+resource_name+" in resources directory")
 
     def __init__(self, illustration_name_list):
         self.illustration_name_list = illustration_name_list
 
+        jquery_file = self.find_resource('jquery')
+        galleria_folder = self.find_resource('galleria')
+
+        self.jquery_loc = os.path.join(DisplayWriter.rendering_resources_foler, jquery_file)
+        self.galleria_loc = os.path.join(DisplayWriter.rendering_resources_foler, galleria_folder, 'src', 'galleria.js')
+        self.galleria_theme_loc = os.path.join(DisplayWriter.rendering_resources_foler, galleria_folder, 'src', 'themes', 'classic')
+        self.html_template = os.path.join(DisplayWriter.rendering_resources_foler, 'slideshow_template.html')
+
 
     def write(self, target_folder):
-        shutil.copyfile(DisplayWriter.jquery_loc, os.path.join(target_folder, 'jquery.js'))
-        shutil.copyfile(DisplayWriter.galleria_loc, os.path.join(target_folder, 'galleria.js'))
-        shutil.copytree(DisplayWriter.galleria_theme_loc, os.path.join(target_folder, 'galleria_classic_theme'))
+        shutil.copyfile(self.jquery_loc, os.path.join(target_folder, 'jquery.js'))
+        shutil.copyfile(self.galleria_loc, os.path.join(target_folder, 'galleria.js'))
+        shutil.copytree(self.galleria_theme_loc, os.path.join(target_folder, 'galleria_classic_theme'))
 
         slideshow_lines = ['          <img src = "' + illustration_name + '">\n' for
                            illustration_name in self.illustration_name_list]
 
-        with open(DisplayWriter.html_template, 'r') as template_file:
+        with open(self.html_template, 'r') as template_file:
             template_lines = template_file.readlines()
 
         insertion_line = next(i for i,v in enumerate(template_lines) if DisplayWriter.image_locator_string in v) + 1
